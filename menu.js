68d82f6e328a35f83309731aaf7e84b16561a0bf
Christian Wiegand <cwiegand@wgdnet.de>
2021-03-18 18:58:56 +0100

    Initial commit

---
+++ menu.js
@@ -0,0 +1,122 @@
+'use strict';
+const path = require('path');
+const {app, Menu, shell} = require('electron');
+const {
+	is,
+	appMenu,
+	aboutMenuItem,
+	openUrlMenuItem
+} = require('electron-util');
+const config = require('./config');
+
+const showPreferences = () => {
+	// Show the app's preferences here
+};
+
+const helpSubmenu = [
+	openUrlMenuItem({
+		label: 'Webseite',
+		url: 'https://eisadler.com'
+	}),
+	openUrlMenuItem({
+		label: 'Source Code',
+		url: 'https://github.com/christianwgd/eisadler-uhr.git'
+	})
+];
+
+if (!is.macos) {
+	helpSubmenu.push(
+		{
+			type: 'separator'
+		},
+		aboutMenuItem({
+			icon: path.join(__dirname, 'static', 'icon.png'),
+			text: 'Created by Your Name'
+		})
+	);
+}
+
+const debugSubmenu = [
+	{
+		label: 'Show Settings',
+		click() {
+			config.openInEditor();
+		}
+	},
+	{
+		label: 'Show App Data',
+		click() {
+			shell.openItem(app.getPath('userData'));
+		}
+	},
+	{
+		type: 'separator'
+	},
+	{
+		label: 'Delete Settings',
+		click() {
+			config.clear();
+			app.relaunch();
+			app.quit();
+		}
+	},
+	{
+		label: 'Delete App Data',
+		click() {
+			shell.moveItemToTrash(app.getPath('userData'));
+			app.relaunch();
+			app.quit();
+		}
+	}
+];
+
+const macosTemplate = [
+	{
+		role: 'fileMenu',
+		submenu: [
+			{
+				role: 'close'
+			}
+		]
+	},
+	{
+		role: 'viewMenu'
+	},
+	{
+		role: 'windowMenu'
+	},
+	{
+		role: 'help',
+		submenu: helpSubmenu
+	}
+];
+
+// Linux and Windows
+const otherTemplate = [
+	{
+		role: 'fileMenu',
+		submenu: [
+			{
+				role: 'quit'
+			}
+		]
+	},
+	{
+		role: 'viewMenu'
+	},
+	{
+		role: 'help',
+		submenu: helpSubmenu
+	}
+];
+
+const template = process.platform === 'darwin' ? macosTemplate : otherTemplate;
+
+if (is.development) {
+	template.push({
+		label: 'Debug',
+		submenu: debugSubmenu
+	});
+}
+
+module.exports = Menu.buildFromTemplate(template);
---
+++ readme.md
@@ -0,0 +1,45 @@
+![Github All Releases](https://img.shields.io/github/downloads/christianwgd/ehvnrw-uhr/total.svg)
+![](https://badgen.net/badge/platform/macos/green?icon=apple)
+![](https://badgen.net/badge/platform/windows/green?icon=windows)
+![](https://badgen.net/badge/platform/linux/green?icon=linux)
+
+# Eisadler Uhr
+
+Desktop App der EHV-NRW Uhr
+
+## Install
+
+*macOS 10.10+, Linux, and Windows 7+ are supported (64-bit only).*
+
+**macOS**
+
+[**Download**](https://github.com/christianwgd/ehvnrw-uhr/releases/latest) the `.dmg` file.
+
+**Linux**
+
+[**Download**](https://github.com/christianwgd/ehvnrw-uhr/releases/latest) the `.AppImage` or `.deb` file.
+
+*The AppImage needs to be [made executable](http://discourse.appimage.org/t/how-to-make-an-appimage-executable/80) after download.*
+
+**Windows**
+
+[**Download**](https://github.com/christianwgd/ehvnrw-uhr/releases/latest) the `.exe` file.
+
+---
+
+## Dev
+
+Built with [Electron](https://electronjs.org).
+
+### Run
+
+```
+$ npm install
+$ npm start
+```
+
+### Publish
+
+```
+$ npm run release
+```
